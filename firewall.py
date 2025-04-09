860882ca9cff4be067f1ad3ac1f456811f6d2aba
Madhav <67517731+Madhav998@users.noreply.github.com>
2025-04-09 10:35:45 +0530

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -144,10 +144,10 @@ http_access deny all
 def update_blocklists():
     logger.info("[INFO] Updating blocklists...")
     urls = {
-        "blocked_ips.txt": "https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/ip-list.txt",  # Example IP blocklist
-        "blocked_ports.txt": "https://github.com/Madhav998/firewall/blob/main/port%20list",  # Example port list
-        "blocked_keywords.txt": "https://github.com/Madhav998/firewall/blob/main/blocked_keywords.txt",  # Replace with your actual URL
-        "blocked_sites.txt": "https://github.com/Madhav998/firewall/blob/main/blocked_sites.txt"  # Replace with your actual URL
+        "blocked_ips.txt": r"https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/ip-list.txt",  # Example IP blocklist
+        "blocked_ports.txt": r"https://github.com/Madhav998/firewall/blob/main/port%20list",  # Example port list
+        "blocked_keywords.txt": r"https://github.com/Madhav998/firewall/blob/main/blocked_keywords.txt",  # Replace with your actual URL
+        "blocked_sites.txt": r"https://github.com/Madhav998/firewall/blob/main/blocked_sites.txt"  # Replace with your actual URL
     }
 
     for filename, url in urls.items():
